5e4641ad50d074554467f44b98825e8168781405
rafag <rafagomezcruz89@gmail.com>
2022-11-25 14:43:27 +0100

    suma

---
+++ holaMundoGit.java
@@ -0,0 +1,12 @@
+package holaMundoGit;
+
+public class holaMundoGit {
+
+	public static void main(String[] args) {
+		int num1 = 2, num2 = 3;
+		System.out.println("La suma de " + num1 + " y " + num2 + " es igual a " +( num1 + num2));
+
+
+	}
+
+}
