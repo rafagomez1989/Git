46fb25d19bf2848f7e814effb977d86ea9fce7e6
rafag <rafagomezcruz89@gmail.com>
2022-11-29 12:40:41 +0100

    Segunda modificación

--- holaMundoGit.java
+++ holaMundoGit.java
@@ -6,6 +6,7 @@ public class holaMundoGit {
 		int num1 = 2, num2 = 3;
 		System.out.println("La suma de " + num1 + " y " + num2 + " es igual a " +( num1 + num2));
 		System.out.println("Modificación 1");
+		System.out.println("Modificación 2");
 
 
 	}
